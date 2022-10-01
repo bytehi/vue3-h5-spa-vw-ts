3a7657521d89824735c0cdecf5db52f0f27d71c3
zhangdongli <zhangdl@dachentech.com.cn>
2022-10-01 14:03:39 +0800

    feat: 添加按需自动引入插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+/**
+ * 组件库按需引入插件
+ * usage: 直接使用组件,无需在任何地方导入组件
+ */
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        // 查找别名
-        alias: [{ find: '@', replacement: '/src' }],
-        // 导入时想要省略的扩展名列表
-        extensions: ['.js', '.vue', '.json', '.scss', '.ts', '*'],
-    },
-});
+  plugins: [vue(), Components({})],
+  resolve: {
+    // 查找别名
+    alias: [{ find: '@', replacement: '/src' }],
+    // 导入时想要省略的扩展名列表
+    extensions: ['.js', '.vue', '.json', '.scss', '.ts', '*'],
+  },
+})
