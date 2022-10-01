e8c1690ea841b75595b6221d18b2b049c8e504e3
zhangdongli <zhangdl@dachentech.com.cn>
2022-10-01 14:49:00 +0800

    feat: 添加element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -5,24 +5,16 @@ import vue from '@vitejs/plugin-vue'
  * usage: 直接使用组件,无需在任何地方导入组件
  */
 import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    terserOptions: {
-      compress: {
-        // 防止 Infinity 被压缩成 1/0，这可能会导致 Chrome 上的性能问题
-        keep_infinity: true,
-        // 生产环境去除 console
-        drop_console: true,
-        // 生产环境去除 debugger
-        drop_debugger: true,
-      },
-    },
-    // chunk 大小警告的限制（以 kbs 为单位）
-    chunkSizeWarningLimit: 1500,
-  },
-  plugins: [vue(), Components({})],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     // 查找别名
     alias: [{ find: '@', replacement: '/src' }],
