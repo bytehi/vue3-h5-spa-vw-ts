9f2e89de98da8909b3d4418827047eac3e37984c
zhangdongli <zhangdl@dachentech.com.cn>
2023-01-13 17:58:54 +0800

    chore: 添加viewpost适配

--- vite.config.ts
+++ vite.config.ts
@@ -17,8 +17,8 @@ import vueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 import WindiCSS from 'vite-plugin-windicss'
 
-import postcssImport from 'postcss-pxtorem'
 import { viteVConsole } from 'vite-plugin-vconsole'
+import postcsspxtoviewport from 'postcss-px-to-viewport'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -60,11 +60,20 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [
-        postcssImport({
-          rootValue({ file }) {
-            return file.indexOf('node_modules/vant') !== -1 ? 37.5 : 75
-          },
-          propList: ['*'],
+        postcsspxtoviewport({
+          unitToConvert: 'px', // 要转化的单位
+          viewportWidth: 750, // UI设计稿的宽度 375、 750
+          unitPrecision: 6, // 转换后的精度，即小数点位数
+          propList: ['*'], // 指定转换的css属性的单位，*代表全部css属性的单位都进行转换
+          viewportUnit: 'vw', // 指定需要转换成的视窗单位，默认vw
+          fontViewportUnit: 'vw', // 指定字体需要转换成的视窗单位，默认vw
+          selectorBlackList: ['ignore-'], // 指定不转换为视窗单位的类名，
+          minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
+          mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
+          replace: true, // 是否转换后直接更换属性值
+          // exclude: [/node_modules/], // 设置忽略文件，用正则做目录名匹配
+          exclude: [],
+          landscape: false, // 是否处理横屏情况
         }),
       ],
     },
