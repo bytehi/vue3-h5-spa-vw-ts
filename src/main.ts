f18a434598397e5bda228c053b8d73feeac84143
zhangdongli <zhangdl@dachentech.com.cn>
2023-01-13 10:33:19 +0800

    fix: 调整样式引入

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,15 @@ import 'virtual:unocss-devtools'
 import 'virtual:windi.css'
 import 'virtual:windi-devtools'
 
-import './style.css'
+/**
+ * 引入函数组件的样式
+ * 见:https://vant-contrib.gitee.io/vant/#/zh-CN/quickstart#4.-yin-ru-han-shu-zu-jian-de-yang-shi
+ */
+import 'vant/es/toast/style'
+import 'vant/es/dialog/style'
+// import 'vant/es/notify/style';
+// import 'vant/es/image-preview/style';
+
+import './styles/index.scss'
 
 createApp(App).use(router).use(store).mount('#app')
